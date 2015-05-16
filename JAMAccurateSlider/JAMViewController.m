49448ad75e99b2ffa4dbb39ba10b2465c2776bc5
Jeff Menter <jmenter@gmail.com>
2015-05-15 21:37:00 -0500

    added swift version of the slider

--- JAMAccurateSlider/JAMViewController.m
+++ JAMAccurateSlider/JAMViewController.m
@@ -1,10 +1,13 @@
 
 #import "JAMViewController.h"
 #import "JAMAccurateSlider.h"
+#import "JAMAccurateSlider-Swift.h"
 
 @interface JAMViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (weak, nonatomic) IBOutlet JAMAccurateSlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet AccurateSlider *slider2;
 
 @end
 
@@ -14,11 +17,17 @@
 {
     [super viewDidLoad];
     [self sliderDidChange:nil];
+    [self slider2Changed:nil];
 }
 
 - (IBAction)sliderDidChange:(id)sender
 {
     self.label.text = [NSString stringWithFormat:@"slider value: %0.3f", self.slider.value];
+}
+
+- (IBAction)slider2Changed:(UISlider *)sender;
+{
+    self.label2.text = [NSString stringWithFormat:@"slider value: %0.3f", self.slider2.value];
 }
 
 @end
